ba65fce041616dc8ac2b50c29a42cce17f101248
Callum-OP <callumoprey@gmail.com>
2025-06-05 21:31:11 +0100

    Added option to create receipt (text file)

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -16,6 +17,8 @@ int main()
     int NumOfChicken = 9, SoldChicken = 0, TotalChicken = 0;
     int NumOfMash = 9, SoldMash = 0, TotalMash = 0;
 
+    ofstream myfile;
+
     while (true) {
         cout<<"\n\t\t\t Please select from the options below";
         cout<<"\n\n 1: Bowl of Cereal";
@@ -25,7 +28,8 @@ int main()
         cout<<"\n 5: Chicken";
         cout<<"\n 6: Mash";
         cout<<"\n 7: Review Order Details";
-        cout<<"\n 8: Exit";
+        cout<<"\n 8: Create Order Receipt";
+        cout<<"\n 9: Exit";
 
         cout<<"\n Enter your choice (by selecting the number of your order): ";
         cin>>choice;
@@ -39,7 +43,7 @@ int main()
                 if (NumOfCereal - SoldCereal >= amount) {
                     SoldCereal = SoldCereal + amount;
                     TotalCereal = TotalCereal + amount*5; // Costs £5 per bowl of cereal
-                    cout<<"\n\n You have ordered "<<amount<<" bowls of cereal, costing "<<TotalCereal;
+                    cout<<"\n\n You have ordered "<<amount<<" bowls of cereal, costing £"<<TotalCereal;
                 }
                 else {
                     cout<<"\n\n Sorry, only "<<NumOfCereal - SoldCereal<<" bowls of cereal remaining";
@@ -55,7 +59,7 @@ int main()
                 if (NumOfPizza - SoldPizza >= amount) {
                     SoldPizza = SoldPizza + amount;
                     TotalPizza = TotalPizza + amount*18; // Costs £18 per pizza
-                    cout<<"\n\n You have ordered "<<amount<<" pizzas, costing "<<TotalPizza;
+                    cout<<"\n\n You have ordered "<<amount<<" pizzas, costing £"<<TotalPizza;
                 }
                 else {
                     cout<<"\n\n Sorry, only "<<NumOfPizza - SoldPizza<<" pizzas remaining";
@@ -71,7 +75,7 @@ int main()
                 if (NumOfFish - SoldFish >= amount) {
                     SoldFish = SoldFish + amount;
                     TotalFish = TotalFish + amount*15; // Costs £15 per fish
-                    cout<<"\n\n You have ordered "<<amount<<" fish, costing "<<TotalFish;
+                    cout<<"\n\n You have ordered "<<amount<<" fish, costing £"<<TotalFish;
                 }
                 else {
                     cout<<"\n\n Sorry, only "<<NumOfFish - SoldFish<<" fish remaining";
@@ -87,7 +91,7 @@ int main()
                 if (NumOfTurkey - SoldTurkey >= amount) {
                     SoldTurkey = SoldTurkey + amount;
                     TotalTurkey = TotalTurkey + amount*18; // Costs £18 per turkey
-                    cout<<"\n\n You have ordered "<<amount<<" turkey, costing "<<TotalTurkey;
+                    cout<<"\n\n You have ordered "<<amount<<" turkey, costing £"<<TotalTurkey;
                 }
                 else {
                     cout<<"\n\n Sorry, only "<<NumOfTurkey - SoldTurkey<<" turkey remaining";
@@ -103,7 +107,7 @@ int main()
                 if (NumOfChicken - SoldChicken >= amount) {
                     SoldChicken= SoldChicken + amount;
                     TotalChicken = TotalChicken + amount*18; // Costs £18 per chicken
-                    cout<<"\n\n You have ordered "<<amount<<" chicken, costing "<<TotalChicken;
+                    cout<<"\n\n You have ordered "<<amount<<" chicken, costing £"<<TotalChicken;
                 }
                 else {
                     cout<<"\n\n Sorry, only "<<NumOfChicken - SoldChicken<<" chicken remaining";
@@ -119,7 +123,7 @@ int main()
                 if (NumOfMash - SoldMash >= amount) {
                     SoldMash = SoldMash + amount;
                     TotalMash = TotalMash + amount*18; // Costs £6 per mash
-                    cout<<"\n\n You have ordered "<<amount<<" mash, costing "<<TotalMash;
+                    cout<<"\n\n You have ordered "<<amount<<" mash, costing £"<<TotalMash;
                 }
                 else {
                     cout<<"\n\n Sorry, only "<<NumOfMash - SoldMash<<" mash remaining";
@@ -129,25 +133,25 @@ int main()
                 break;
             case 7:
                 if (TotalCereal != 0) {
-                    cout<<"\n You have ordered "<<SoldCereal<<" bowls of cereal, costing "<<TotalCereal;
+                    cout<<"\n You have ordered "<<SoldCereal<<" bowls of cereal, costing £"<<TotalCereal;
                 }
                 if (TotalPizza != 0) {
-                    cout<<"\n You have ordered "<<SoldPizza<<" pizzas, costing "<<TotalPizza;
+                    cout<<"\n You have ordered "<<SoldPizza<<" pizzas, costing £"<<TotalPizza;
                 }
                 if (TotalFish != 0) {
-                    cout<<"\n You have ordered "<<SoldFish<<" fish, costing "<<TotalFish;
+                    cout<<"\n You have ordered "<<SoldFish<<" fish, costing £"<<TotalFish;
                 }
                 if (TotalTurkey != 0) {
-                    cout<<"\n You have ordered "<<SoldTurkey<<" turkey, costing "<<TotalTurkey;
+                    cout<<"\n You have ordered "<<SoldTurkey<<" turkey, costing £"<<TotalTurkey;
                 }
                 if (TotalChicken != 0) {
-                    cout<<"\n You have ordered "<<SoldChicken<<" chicken, costing "<<TotalChicken;
+                    cout<<"\n You have ordered "<<SoldChicken<<" chicken, costing £"<<TotalChicken;
                 }
                 if (TotalMash != 0) {
-                    cout<<"\n You have ordered "<<SoldMash<<" mash, costing "<<TotalMash;
+                    cout<<"\n You have ordered "<<SoldMash<<" mash, costing £"<<TotalMash;
                 }
                 if (TotalCereal + TotalPizza + TotalFish + TotalTurkey + TotalChicken + TotalMash != 0) {
-                    cout<<"\n\n Total cost: "<<TotalCereal + TotalPizza + TotalFish + TotalTurkey + TotalChicken + TotalMash;
+                    cout<<"\n\n Total cost: £"<<TotalCereal + TotalPizza + TotalFish + TotalTurkey + TotalChicken + TotalMash;
                 } else {
                      cout<<"\n\n You haven't ordered anything yet";
                 }
@@ -155,6 +159,35 @@ int main()
                 cin>>any;
                 break;
             case 8:
+                myfile.open ("Receipt.txt");
+                if (TotalCereal != 0) {
+                    myfile<<"\n "<<SoldCereal<<" bowls of cereal, £"<<TotalCereal;
+                }
+                if (TotalPizza != 0) {
+                    myfile<<"\n "<<SoldPizza<<" pizzas: £"<<TotalPizza;
+                }
+                if (TotalFish != 0) {
+                    myfile<<"\n "<<SoldFish<<" fish: £"<<TotalFish;
+                }
+                if (TotalTurkey != 0) {
+                    myfile<<"\n "<<SoldTurkey<<" turkey: £"<<TotalTurkey;
+                }
+                if (TotalChicken != 0) {
+                    myfile<<"\n "<<SoldChicken<<" chicken: £"<<TotalChicken;
+                }
+                if (TotalMash != 0) {
+                    myfile<<"\n "<<SoldMash<<" mash: £"<<TotalMash;
+                }
+                if (TotalCereal + TotalPizza + TotalFish + TotalTurkey + TotalChicken + TotalMash != 0) {
+                    myfile<<"\n\n Total cost: £"<<TotalCereal + TotalPizza + TotalFish + TotalTurkey + TotalChicken + TotalMash;
+                } else {
+                     cout<<"\n\n You haven't ordered anything yet";
+                }
+                myfile.close();
+                cout<<"\n\n Press c key to continue ";
+                cin>>any;
+                break;
+            case 9:
                 exit(0);
                 return 0;
             default:
